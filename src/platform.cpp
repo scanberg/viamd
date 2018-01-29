5d9fa63f57bc02cdf63080a7d80a2247b29d6e5a
Robin Skånberg <robin.skanberg@liu.se>
2018-01-29 13:34:04 +0100

    Changes to git structure

--- src/platform.cpp
+++ src/platform.cpp
@@ -1,5 +1,11 @@
 #include "platform.h"
 #include <GLFW/glfw3.h>
+#ifdef _WIN32
+#undef APIENTRY
+#define GLFW_EXPOSE_NATIVE_WIN32
+#define GLFW_EXPOSE_NATIVE_WGL
+#include <GLFW/glfw3native.h>
+#endif
 #include <imgui.h>
 #include <stdio.h>
 #include <stdlib.h>
