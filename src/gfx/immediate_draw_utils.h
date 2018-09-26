a95c37bc201e0ed6b9f96195b7dfcbef1ccc8c9d
Robin Skånberg <scanberg@gmail.com>
2018-09-26 06:32:40 +0200

    Moved cxx defines in cmakelists to propagate it to depending projects, changed constexpr vec4 types back to const since vec4 constexpr constructor is not available on clang

--- src/gfx/immediate_draw_utils.h
+++ src/gfx/immediate_draw_utils.h
@@ -23,32 +23,32 @@ void set_material(const Material& mat);
 
 void flush();
 
-constexpr vec4 COLOR_WHITE = {1, 1, 1, 1};
-constexpr vec4 COLOR_BLACK = {0, 0, 0, 1};
-constexpr vec4 COLOR_RED = {1, 0, 0, 1};
-constexpr vec4 COLOR_GREEN = {0, 1, 0, 1};
-constexpr vec4 COLOR_BLUE = {0, 0, 1, 1};
-constexpr vec4 COLOR_YELLOW = {1, 1, 0, 1};
-constexpr vec4 COLOR_MAGENTA = {1, 0, 1, 1};
-constexpr vec4 COLOR_CYAN = {0, 1, 1, 1};
+const vec4 COLOR_WHITE = {1, 1, 1, 1};
+const vec4 COLOR_BLACK = {0, 0, 0, 1};
+const vec4 COLOR_RED = {1, 0, 0, 1};
+const vec4 COLOR_GREEN = {0, 1, 0, 1};
+const vec4 COLOR_BLUE = {0, 0, 1, 1};
+const vec4 COLOR_YELLOW = {1, 1, 0, 1};
+const vec4 COLOR_MAGENTA = {1, 0, 1, 1};
+const vec4 COLOR_CYAN = {0, 1, 1, 1};
 
-constexpr Material MATERIAL_ROUGH_WHITE = {COLOR_WHITE};
-constexpr Material MATERIAL_ROUGH_BLACK = {COLOR_BLACK};
-constexpr Material MATERIAL_ROUGH_RED = {COLOR_RED};
-constexpr Material MATERIAL_ROUGH_GREEN = {COLOR_GREEN};
-constexpr Material MATERIAL_ROUGH_BLUE = {COLOR_BLUE};
-constexpr Material MATERIAL_ROUGH_YELLOW = {COLOR_YELLOW};
-constexpr Material MATERIAL_ROUGH_MAGENTA = {COLOR_MAGENTA};
-constexpr Material MATERIAL_ROUGH_CYAN = {COLOR_CYAN};
+const Material MATERIAL_ROUGH_WHITE = {COLOR_WHITE};
+const Material MATERIAL_ROUGH_BLACK = {COLOR_BLACK};
+const Material MATERIAL_ROUGH_RED = {COLOR_RED};
+const Material MATERIAL_ROUGH_GREEN = {COLOR_GREEN};
+const Material MATERIAL_ROUGH_BLUE = {COLOR_BLUE};
+const Material MATERIAL_ROUGH_YELLOW = {COLOR_YELLOW};
+const Material MATERIAL_ROUGH_MAGENTA = {COLOR_MAGENTA};
+const Material MATERIAL_ROUGH_CYAN = {COLOR_CYAN};
 
-constexpr Material MATERIAL_GLOSSY_WHITE = {COLOR_WHITE, {0.04f, 0.04f, 0.04f}, 0.5};
-constexpr Material MATERIAL_GLOSSY_BLACK = {COLOR_BLACK, {0.04f, 0.04f, 0.04f}, 0.5};
-constexpr Material MATERIAL_GLOSSY_RED = {COLOR_RED, {0.04f, 0.04f, 0.04f}, 0.5};
-constexpr Material MATERIAL_GLOSSY_GREEN = {COLOR_GREEN, {0.04f, 0.04f, 0.04f}, 0.5};
-constexpr Material MATERIAL_GLOSSY_BLUE = {COLOR_BLUE, {0.04f, 0.04f, 0.04f}, 0.5};
-constexpr Material MATERIAL_GLOSSY_YELLOW = {COLOR_YELLOW, {0.04f, 0.04f, 0.04f}, 0.5};
-constexpr Material MATERIAL_GLOSSY_MAGENTA = {COLOR_MAGENTA, {0.04f, 0.04f, 0.04f}, 0.5};
-constexpr Material MATERIAL_GLOSSY_CYAN = {COLOR_CYAN, {0.04f, 0.04f, 0.04f}, 0.5};
+const Material MATERIAL_GLOSSY_WHITE = {COLOR_WHITE, {0.04f, 0.04f, 0.04f}, 0.5};
+const Material MATERIAL_GLOSSY_BLACK = {COLOR_BLACK, {0.04f, 0.04f, 0.04f}, 0.5};
+const Material MATERIAL_GLOSSY_RED = {COLOR_RED, {0.04f, 0.04f, 0.04f}, 0.5};
+const Material MATERIAL_GLOSSY_GREEN = {COLOR_GREEN, {0.04f, 0.04f, 0.04f}, 0.5};
+const Material MATERIAL_GLOSSY_BLUE = {COLOR_BLUE, {0.04f, 0.04f, 0.04f}, 0.5};
+const Material MATERIAL_GLOSSY_YELLOW = {COLOR_YELLOW, {0.04f, 0.04f, 0.04f}, 0.5};
+const Material MATERIAL_GLOSSY_MAGENTA = {COLOR_MAGENTA, {0.04f, 0.04f, 0.04f}, 0.5};
+const Material MATERIAL_GLOSSY_CYAN = {COLOR_CYAN, {0.04f, 0.04f, 0.04f}, 0.5};
 
 // Primitives
 
