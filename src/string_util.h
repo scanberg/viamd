10c1827399276a382ef74e255367c626eced9732
Robin Skånberg <scanberg@gmail.com>
2021-10-10 22:50:49 +0200

    Removed static member MaxSize in StrBuf, trying to make ApplicationData standard layout compliant...

--- src/string_util.h
+++ src/string_util.h
@@ -47,28 +47,27 @@ namespace cexpr_helper {
 // A buf string for wrapping a char buf[N]
 template <int64_t Size>
 struct StrBuf {
-    static constexpr int64_t MaxSize = Size;
-    static_assert(MaxSize > 1, "Size of StrBuf must be more than 1");
-    char buf[MaxSize] = {};
+    static_assert(Size > 1, "Size of StrBuf must be more than 1");
+    char buf[Size] = {};
 
     constexpr StrBuf() noexcept {};
 
     template <int64_t N>
     constexpr StrBuf(const char (&cstr)[N]) noexcept  {
-        constexpr auto length = N < MaxSize ? N : MaxSize - 1;
+        constexpr auto length = N < Size ? N : Size - 1;
         cexpr_helper::cexpr_copy(buf, cstr, length);
         buf[length] = '\0';
     }
 
     constexpr StrBuf(const char* cstr) noexcept {
-        int64_t len = cexpr_helper::cexpr_strnlen(cstr, MaxSize - 1);
+        int64_t len = cexpr_helper::cexpr_strnlen(cstr, Size - 1);
         cexpr_helper::cexpr_copy(buf, cstr, len);
         buf[len] = '\0';
     }
 
     constexpr StrBuf(const char* cstr, int64_t len) noexcept {
         ASSERT(len > 0);
-        len = len < MaxSize - 1 ? len : MaxSize - 1;
+        len = len < Size - 1 ? len : Size - 1;
         cexpr_helper::cexpr_copy(buf, cstr, len);
         buf[len] = '\0';
     }
@@ -79,26 +78,26 @@ struct StrBuf {
     }
 
     constexpr StrBuf(const StrBuf& other) noexcept {
-        cexpr_helper::cexpr_copy(buf, other.buf, MaxSize);
+        cexpr_helper::cexpr_copy(buf, other.buf, Size);
         //memcpy(buf, other.buf, MaxSize);
-        buf[MaxSize - 1] = '\0';
+        buf[Size - 1] = '\0';
     }
 
     template <int64_t N>
     constexpr StrBuf(const StrBuf<N>& other) noexcept {
-        constexpr auto len = N < MaxSize ? N : MaxSize;
+        constexpr auto len = N < Size ? N : Size;
         cexpr_helper::cexpr_copy(buf, other.buf, len);
         buf[len - 1] = '\0';
     }
 
     constexpr StrBuf(StrBuf&& other) noexcept {
-        cexpr_helper::cexpr_copy(buf, other.buf, MaxSize);
-        buf[MaxSize - 1] = '\0';
+        cexpr_helper::cexpr_copy(buf, other.buf, Size);
+        buf[Size - 1] = '\0';
     }
 
     template <int64_t N>
     constexpr StrBuf(StrBuf<N>&& other) noexcept {
-        constexpr auto len = N < MaxSize ? N : MaxSize;
+        constexpr auto len = N < Size ? N : Size;
         cexpr_helper::cexpr_copy(buf, other.buf, len);
         buf[len - 1] = '\0';
     }
@@ -107,8 +106,8 @@ struct StrBuf {
 
     constexpr StrBuf& operator=(const StrBuf& other) noexcept {
         if (this != &other) {
-            cexpr_helper::cexpr_copy(buf, other.buf, MaxSize);
-            buf[MaxSize - 1] = '\0';
+            cexpr_helper::cexpr_copy(buf, other.buf, Size);
+            buf[Size - 1] = '\0';
         }
         return *this;
     }
@@ -116,7 +115,7 @@ struct StrBuf {
     template <int64_t N>
     constexpr StrBuf& operator=(const StrBuf<N>& other) noexcept {
         if (this != &other) {
-            constexpr auto len = N < (MaxSize - 1) ? N : (MaxSize - 1);
+            constexpr auto len = N < (Size - 1) ? N : (Size - 1);
             cexpr_helper::cexpr_copy(buf, other.buf, len);
             buf[len] = '\0';
         }
@@ -134,7 +133,7 @@ struct StrBuf {
     template <int64_t N>
     constexpr StrBuf& operator=(const char (&cstr)[N]) noexcept {
         if (&buf[0] != &cstr[0]) {
-            constexpr auto len = N < (MaxSize - 1) ? N : (MaxSize - 1);
+            constexpr auto len = N < (Size - 1) ? N : (Size - 1);
             cexpr_helper::cexpr_copy(buf, cstr, len);
             buf[len] = '\0';
         }
@@ -142,7 +141,7 @@ struct StrBuf {
     }
 
     constexpr StrBuf& operator=(const char* cstr) noexcept {
-        const int64_t len = cexpr_helper::cexpr_strnlen(cstr, MaxSize - 1);
+        const int64_t len = cexpr_helper::cexpr_strnlen(cstr, Size - 1);
         cexpr_helper::cexpr_copy(buf, cstr, len);
         buf[len] = '\0';
         return *this;
@@ -151,43 +150,43 @@ struct StrBuf {
     StrBuf& operator+=(str_t txt);
 
     constexpr char operator[](int64_t i) const noexcept {
-        ASSERT(0 <= i && i < MaxSize);
+        ASSERT(0 <= i && i < Size);
         return buf[i];
     }
 
     constexpr char& operator[](int64_t i) noexcept {
-        ASSERT(0 <= i && i < MaxSize);
+        ASSERT(0 <= i && i < Size);
         return buf[i];
     }
 
     constexpr operator bool() const noexcept { return buf[0] != '\0'; }
 
-    constexpr int64_t capacity() const noexcept { return MaxSize; }
-    constexpr int64_t length() const noexcept { return cexpr_helper::cexpr_strnlen(buf, MaxSize); }
+    constexpr int64_t capacity() const noexcept { return Size; }
+    constexpr int64_t length() const noexcept { return cexpr_helper::cexpr_strnlen(buf, Size); }
 
     constexpr char* cstr() const noexcept { return (char*)buf; }
     constexpr operator str_t() const noexcept { return {buf, length()}; }
 
     constexpr const char* begin() const noexcept { return buf; }
     constexpr const char* beg() const noexcept { return buf; }
-    constexpr const char* end() const noexcept { return buf + MaxSize; }
+    constexpr const char* end() const noexcept { return buf + Size; }
 
     constexpr char* begin() noexcept { return buf; }
     constexpr char* beg() noexcept { return buf; }
-    constexpr char* end() noexcept { return buf + MaxSize; }
+    constexpr char* end() noexcept { return buf + Size; }
 };
 
 template <int64_t N>
 StrBuf<N>::StrBuf(str_t str) {
     // @NOTE: MAX_LENGTH - 1 here because we copy from cstring which excludes \0
-    auto len = str.len < (MaxSize - 1) ? str.len : (MaxSize - 1);
+    auto len = str.len < (N - 1) ? str.len : (N - 1);
     cexpr_helper::cexpr_copy(buf, str.ptr, len);
     buf[len] = '\0';
 }
 
 template <int64_t N>
 StrBuf<N>& StrBuf<N>::operator=(str_t str) {
-    auto len = str.len < (MaxSize - 1) ? str.len : (MaxSize - 1);
+    auto len = str.len < (N - 1) ? str.len : (N - 1);
     cexpr_helper::cexpr_copy(buf, str.ptr, len);
     buf[len] = '\0';
     return *this;
@@ -195,11 +194,11 @@ StrBuf<N>& StrBuf<N>::operator=(str_t str) {
 
 template <int64_t N>
 StrBuf<N>& StrBuf<N>::operator+=(str_t txt) {
-    const int64_t offset = (int64_t)cexpr_helper::cexpr_strnlen((const char*)buf, MaxSize);
-    const int64_t length = MaxSize - offset;
+    const int64_t offset = (int64_t)cexpr_helper::cexpr_strnlen((const char*)buf, N);
+    const int64_t length = N - offset;
     if (length > 0) {
         cexpr_helper::cexpr_strncpy((char*)buf + offset, txt.ptr, txt.len < length ? txt.len : length);
     }
-    buf[MaxSize - 1] = '\0';
+    buf[N - 1] = '\0';
     return *this;
 }
