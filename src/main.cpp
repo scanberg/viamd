8eb293ad9ea4deb08d6f50ce2bf5eb21baae7b78
scanberg <scanberg@gmail.com>
2025-04-15 07:26:44 +0200

    Added log info of elapsed time when calculating trajectory data.

--- src/main.cpp
+++ src/main.cpp
@@ -7405,14 +7405,17 @@ static void init_trajectory_data(ApplicationState* data) {
                 }
             });
 
-            task_system::ID main_task = task_system::create_main_task(STR_LIT("Update Trajectory Data"), [data]() {
+            uint64_t time = (uint64_t)md_time_current();
+            task_system::ID main_task = task_system::create_main_task(STR_LIT("Update Trajectory Data"), [data, t0 = time]() {
+                uint64_t t1 = (uint64_t)md_time_current();
+                double elapsed = md_time_as_seconds(t1 - t0);
+                MD_LOG_INFO("Finished computing trajectory data (%.2fs)", elapsed);
                 data->trajectory_data.backbone_angles.fingerprint = generate_fingerprint();
                 data->trajectory_data.secondary_structure.fingerprint = generate_fingerprint();
                 
                 interpolate_atomic_properties(data);
                 data->mold.dirty_buffers |= MolBit_ClearVelocity;
                 update_all_representations(data);
-
             });
 
             task_system::set_task_dependency(main_task, data->tasks.backbone_computations);
